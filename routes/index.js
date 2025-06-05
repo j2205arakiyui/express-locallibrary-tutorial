4072a93fdbe8424ccb58e30f0321bda99c2e4fc2
j2205arakiyui <j2205@matsue-ct.ac.jp>
2025-06-05 07:56:25 +0000

    ようこそ２

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res) {
-  res.redirect("index",{title:"地域図書館へようこそ"});
+  res.redirect("index",{title:"地域図書館ホーム"});
 });
 
 module.exports = router;
